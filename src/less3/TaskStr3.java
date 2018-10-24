1ab6bf761af19c1fe5dbd67c3705f42c34dc86df
Zhgutova <tzhgutova@gmail.com>
2018-10-24 16:09:53 +0300

    Task3 корректировка

--- src/less3/TaskStr3.java
+++ src/less3/TaskStr3.java
@@ -14,37 +14,44 @@ public class TaskStr3 {
 		Scanner sc = new Scanner (System.in);
 		System.out.println("Введите текст");
 		String str = sc.nextLine();
+		sc.close();
 		String tmp = "";
 		int tmp_int = 0;
 		int[] tmp_arr, arr;
 		tmp_arr = new int[str.length()];
 		int count = 0;
-		
-		for (int i = 0; i < str.length(); i++)
+
+		for (int i = 0; i < str.length(); i++) 
 		{
-			if (i + 1 == str.length()) System.out.println("!!!");
+			//если текущий символ - цифра, к временной строки добавляем его
 			if (Character.isDigit (str.charAt(i))) 
-		 {
-		//	 count++;
-			 tmp+=str.charAt(i);
-			 System.out.println(i + " tmp " + tmp);
-		 }
-		 
-		 else {
-			  if (tmp != "") 
-			  	{ 
+				 tmp+=str.charAt(i);
+			//если текущий символ не цифра и временная строка не пуста, преобразуем строку в число
+			//и записываем его в текущий элемент временнрого массива
+			//в count считаем кол-во элементов для целочисленного массива
+			//после обнуляем временную строку
+			else if (tmp != "") 
+			{
 				  tmp_int = Integer.parseInt(tmp);
 				  tmp_arr [count] = tmp_int;
 				  count++;
-			 	  System.out.println("tmp_imt " + tmp_int);
-			 	tmp = "";
-		//	 	tmp_int = 0;
-			 		}
-		}}
-		System.out.println(count);
-		//System.out.println(Arrays.toString(tmp_arr));
-		
+			 	//  System.out.println("tmp_imt " + tmp_int);
+			 	  tmp = "";
+
+			}
+			//если дошли до конца строки и временная строка не пуста, делаем описанное выше
+			if ((i == str.length()-1)&&(tmp != ""))
+			{
+					  tmp_int = Integer.parseInt(tmp);
+					  tmp_arr [count] = tmp_int;
+					  count++;
+				 //	  System.out.println("tmp_imt " + tmp_int);
+			}
+
+		}	
+		// создаем новый массив с кол-вом найденных в строке чисел 
 		arr = new int[count];
+		// копируем из временного массива в целевой найденные числа
 		System.arraycopy(tmp_arr, 0, arr, 0, count);
 		System.out.println(Arrays.toString(arr));
 	}
